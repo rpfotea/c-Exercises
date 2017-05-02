565b2ac3652bf90155ad8c0ab682ab66a9a84fb7
Radu Fotea <petru.fotea@gmail.com>
2017-05-02 11:54:37 -0400

    Initial commit

---
+++ Exercises.Tests/FizzBuzzTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void GoFizz()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("1", fizzBuzz.GoFizz(1));
+            Assert.AreEqual("Buzz", fizzBuzz.GoFizz(10));
+            Assert.AreEqual("Fizz", fizzBuzz.GoFizz(3));
+            Assert.AreEqual("Buzz", fizzBuzz.GoFizz(5));
+            Assert.AreEqual("FizzBuzz", fizzBuzz.GoFizz(15));
+            Assert.AreEqual("FizzBuzz", fizzBuzz.GoFizz(30));
+        }
+    }
+}
---
+++ Exercises/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class FizzBuzz
+    {
+        public string GoFizz(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (n % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (n % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return n.ToString();
+            }
+        }
+    }
+}
