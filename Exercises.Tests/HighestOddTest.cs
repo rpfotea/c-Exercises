4935346a7f42eee48538cea2793a7e71028ee11a
Radu Fotea <petru.fotea@gmail.com>
2017-05-03 10:49:11 -0400

    add ex Highest odd number

---
+++ Exercises.Tests/HighestOddTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class HighestOddT
+    {
+        [TestMethod]
+        public void HighestOddTest()
+        {
+            //arrange
+            GoHighestOdd highestOdd = new GoHighestOdd();
+            int[] nums1 = new int[0];
+            int[] nums2 = new int[2] { 2, 10 };
+            int[] nums3 = new int[3] { 3, 7, 8 };
+            int[] nums4 = new int[4] { 0, 9, 8, 7 };
+            //act
+
+            //assert
+            Assert.AreEqual(0, highestOdd.HighestOdd(nums1));
+            Assert.AreEqual(0, highestOdd.HighestOdd(nums2));
+            Assert.AreEqual(7, highestOdd.HighestOdd(nums3));
+            Assert.AreEqual(9, highestOdd.HighestOdd(nums4));
+        }
+    }
+}
---
+++ Exercises/HighestOdd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class GoHighestOdd
+    {
+        /*
+         * Given an array of positive integers, find the highest odd number. If there aren't any odd
+         * numbers, return back 0.
+         * 
+         * HighestOdd([]) -> 0
+         * HighestOdd([2, 10]) -> 0
+         * HighestOdd([3, 7, 8]) -> 7
+         */
+        public int HighestOdd(int[] nums)
+        {
+            int highestOddNumber = 0;
+            if (nums.Length==0)
+            {
+                highestOddNumber=0;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i]%2==1 && nums[i]>highestOddNumber)
+                {
+                    highestOddNumber = nums[i];
+                }
+            }
+            return highestOddNumber;
+        }
+    }
+}
