71a8219ba60188ddeddef3233d61a6f1e06c0314
Radu Fotea <petru.fotea@gmail.com>
2017-05-03 11:37:20 -0400

    Add ex second highest number + test

---
+++ Exercises.Tests/HighestSecondTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class HighestSecondT
+    {
+        [TestMethod]
+        public void HighestSecondTerst()
+        {
+            //arrange
+            GoSecondHighest secondHigh = new GoSecondHighest();
+            int[] nums1 = new int[0];
+            int[] nums2 = new int[1] { 3 };
+            int[] nums3 = new int[3] {1, 3, 5 };
+            int[] nums4 = new int[3] { 1, 3, 3 };
+            int[] nums5 = new int[6] { 1, 4, 6, 8, 23, 13 };
+            int[] nums6 = new int[7] { 1, -2, 6, 14, 23, -13, 13 };
+            //act
+
+            //assert
+            Assert.AreEqual(0, secondHigh.SecondHighest(nums1));
+            Assert.AreEqual(3, secondHigh.SecondHighest(nums2));
+            Assert.AreEqual(3, secondHigh.SecondHighest(nums3));
+            Assert.AreEqual(3, secondHigh.SecondHighest(nums4));
+            Assert.AreEqual(13, secondHigh.SecondHighest(nums5));
+            Assert.AreEqual(14, secondHigh.SecondHighest(nums6));
+        }
+    }
+}
---
+++ Exercises/SecondHighest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class GoSecondHighest
+    {
+        /*
+        * CHALLENGE: Given an array of integers, return the second highest number.
+        * For arrays of length 0 retun 0, length 1 return the highest number.
+        * 
+        * SecondHighest([]) -> 0
+        * SecondHighest([1]) -> 1
+        * SecondHighest([1, 3, 5]) -> 3
+        * SecondHighest([1, 3, 3]) -> 3
+        * SecondHighest([1, 4, 6, 8, 23, 13]) -> 13
+        */
+        public int SecondHighest(int[] nums)
+        {
+            int secondNumber = 0;
+            
+            if(nums.Length==0)
+            {
+                secondNumber = 0;
+            }
+
+            if(nums.Length==1)
+            {
+                secondNumber =nums[0];
+            }
+            if(nums.Length>1)
+            {
+                Array.Sort(nums);
+                secondNumber = nums[nums.Length - 2];
+            }
+            return secondNumber;
+        }
+
+    }
+}
