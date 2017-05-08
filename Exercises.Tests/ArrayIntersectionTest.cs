467e91a3a80d46bb179a1d8f976385a80f298f87
Radu Fotea <petru.fotea@gmail.com>
2017-05-08 15:28:59 -0400

    Add ArrayIntesection + test

---
+++ Exercises.Tests/ArrayIntersectionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class ArrayIntersectionT
+    {
+        [TestMethod]
+        public void ArrayIntersectiontest()
+        {
+            //arrange
+            Exercises newIntersection = new Exercises();
+            
+            int[] a1 = new int[3]{ 1, 3, 5 };
+            int[] b1 = new int[3]{ 1, 5, 7, };
+            int[] r1 = new int[2] { 1, 5 };
+                        
+            int[] a2 = new int[1] { 1 };
+            int[] b2 = new int[3] { 1, 5, 7, };
+            int[] r2 = new int[1] { 1 };
+
+            int[] a3 = new int[0] { };
+            int[] b3 = new int[0] { };
+            int[] r3 = new int[0] { };
+            //ArrayIntersection([1, 1, 1], [1, 5, 7]) → [1]
+            int[] a4 = new int[3] { 1, 1, 1 };
+            int[] b4 = new int[3] { 1, 5, 7, };
+            int[] r4 = new int[1] { 1};
+            //act
+
+            //assert
+            CollectionAssert.AreEqual(r1, newIntersection.ArrayIntersection(a1, b1));
+            CollectionAssert.AreEqual(r2, newIntersection.ArrayIntersection(a2, b2));
+            CollectionAssert.AreEqual(r3, newIntersection.ArrayIntersection(a3, b3));
+            CollectionAssert.AreEqual(r4, newIntersection.ArrayIntersection(a4, b4));
+        }
+    }
+}
---
+++ Exercises/ArrayIntersection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+        CHALLENGE: Using array A and array B, return an array that is the intersection of both A and B.
+        The intersection includes numbers that appear in both arrays.
+
+            ArrayIntersection([1, 3, 5], [1, 5, 7]) → [1, 5]
+            ArrayIntersection([1], [1, 5, 7]) → [1]
+            ArrayIntersection([1, 1, 1], [1, 5, 7]) → [1]
+            ArrayIntersection([], []) → []
+        */
+        public int[] ArrayIntersection(int[] a, int[] b)
+        {
+            HashSet<int> result = new HashSet<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if(b[j]==a[i])
+                    {
+                        result.Add(b[j]);
+                    }
+                }
+            }
+
+            int[] newArray = new int[result.Count];
+
+            result.CopyTo(newArray);
+
+            return newArray;
+        }
+    }
+}
