dff24b38b3b46bbba3ede6416f03179c502c1e21
Radu Fotea <petru.fotea@gmail.com>
2017-05-09 16:16:57 -0400

    countClamps exerc + test

---
+++ Exercises.Tests/CountClampsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class CountClampsT
+    {
+        [TestMethod]
+        public void CountClamsTest()
+        {
+            Exercises newClamps = new Exercises();
+            int[] set1 = new int[6] { 1, 2, 2, 3, 4, 4 };
+            int[] set2 = new int[5] { 1, 1, 2, 1, 1 };
+            int[] set3 = new int[5] { 1, 1, 1, 1, 1 };
+
+            Assert.AreEqual(2, newClamps.CountClumps(set1));
+            Assert.AreEqual(2, newClamps.CountClumps(set2));
+            Assert.AreEqual(1, newClamps.CountClumps(set3));
+        }
+    }
+}
---
+++ Exercises/CountClamps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+        CHALLENGE: Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value.
+        Return the number of clumps in the given array.
+
+        countClumps([1, 2, 2, 3, 4, 4]) → 2
+        countClumps([1, 1, 2, 1, 1]) → 2
+        countClumps([1, 1, 1, 1, 1]) → 1
+        */
+        public int CountClumps(int[] nums)
+        {
+            int setClamps = 0;
+            bool moreClams = false;
+            int numbSetClamps = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if(nums[i]==nums[i-1])
+                {
+                    moreClams = true;
+                    numbSetClamps += 1;
+                }
+                else
+                {
+                    if (moreClams==true)
+                    {
+                        setClamps += 1;
+                        numbSetClamps = 0;
+                    }
+                }
+            }
+            if(numbSetClamps>0 & setClamps==0)
+            {
+                setClamps += 1;
+            }
+            return setClamps;
+        }
+    }
+}
