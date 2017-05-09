42aa23a06bb0de42e6fdb402b001970b2809548a
Radu Fotea <petru.fotea@gmail.com>
2017-05-09 15:41:57 -0400

    add closeFar exercise + test

---
+++ Exercises.Tests/CloseFarTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class CloseFarT
+    {
+        [TestMethod]
+        public void CloseFarTest()
+        {
+            Exercises closeF = new Exercises();
+
+            Assert.AreEqual(true, closeF.CloseFar(1, 2, 12));
+            Assert.AreEqual(false, closeF.CloseFar(1, 2,3));
+            Assert.AreEqual(true, closeF.CloseFar(4, 1, 3));
+        }
+    }
+}
---
+++ Exercises/CloseFar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+
+        /*
+        Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), 
+        while the other is "far", differing from both other values by 2 or more.
+        Note: Math.abs(num) computes the absolute value of a number.
+
+        closeFar(1, 2, 10) → true
+        closeFar(1, 2, 3) → false
+        closeFar(4, 1, 3) → true
+        */
+        public bool CloseFar(int a, int b, int c)
+        {
+            if((Math.Abs(a-b)<2 && Math.Abs(a-c)>1 && Math.Abs(b-c)>1) || (Math.Abs(a - c) < 2 && Math.Abs(a - b) > 1 && Math.Abs(b - c) > 1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
