34ebc10df5ae89e3fad448cafe2d4aecf84de804
Radu Fotea <petru.fotea@gmail.com>
2017-05-12 12:27:19 -0400

    add Has271 exercise + test

---
+++ Exercises.Tests/Has271Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class Has271T
+    {
+        [TestMethod]
+        public void Has271Test()
+        {
+            Exercises has271 = new Exercises();
+            int[] set1 = new int[4] {1,2,7,1 };
+            int[] set2 = new int[5] { 3, 8, 2, 1, 9 };
+            int[] set3 = new int[4] { 1, 2, 8, 1 };
+            int[] set4 = new int[3] { -2, -7, -1 };
+
+            Assert.AreEqual(true, has271.Has271(set1));
+            Assert.AreEqual(true, has271.Has271(set2));
+            Assert.AreEqual(false, has271.Has271(set3));
+            Assert.AreEqual(true, has271.Has271(set4));
+        }
+    }
+}
---
+++ Exercises/Has271.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+        * Given an array of ints, return true if it contains a 2, 7, 1 pattern -- a value, followed by 
+        * the value plus 5, followed by the value minus 1. 
+        * Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+        * 
+        * has271([1, 2, 7, 1]) → true
+        * has271([1, 2, 8, 1]) → false
+        * has271([2, 7, 1]) → true     
+        */
+        public bool Has271(int[] nums)
+        {
+            bool hasPattern = false;
+            if (nums.Length > 2)
+            {
+                for (int i = 0; i < nums.Length - 2; i++)
+                {
+                    if ((Math.Abs(nums[i]) == Math.Abs(nums[i + 1]) - 5) && (Math.Abs(nums[i]) == Math.Abs(nums[i + 2]) + 1))
+                    {
+                        hasPattern=true;
+                    }
+                }
+            }
+
+            return hasPattern;
+        }
+    }
+}
