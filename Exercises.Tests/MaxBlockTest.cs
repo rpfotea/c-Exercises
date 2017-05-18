46c0aafb1fe47e33da0a95deae420c9dd6d8ba6d
Radu Fotea <petru.fotea@gmail.com>
2017-05-18 10:18:09 -0400

    Add MaxBlock exerc. + Test

---
+++ Exercises.Tests/MaxBlockTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class MaxBlockT
+    {
+        [TestMethod]
+        public void MaxBlockTest()
+        {
+            Exercises maxBlock = new Exercises();
+
+            Assert.AreEqual(0, maxBlock.MaxBlock(""));
+            Assert.AreEqual(1, maxBlock.MaxBlock("a"));
+            Assert.AreEqual(2, maxBlock.MaxBlock("hoopla"));
+            Assert.AreEqual(3, maxBlock.MaxBlock("abbCCCddBBBxx"));
+        }
+    }
+}
---
+++ Exercises/MaxBlock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+
+        /*
+        CHALLENGE: Given a string, return the length of the largest "block" in the string. 
+        A block is a run of adjacent chars that are the same.
+
+        maxBlock("hoopla") → 2
+        maxBlock("abbCCCddBBBxx") → 3
+        maxBlock("") → 0
+        */
+        public int MaxBlock(string str)
+        {
+            int maxLength = 0;
+            int actualMaxLength = 1;
+            int currentLength = 1;
+            if (str.Length == 1)
+            {
+                maxLength = currentLength;
+            }
+            if (str.Length > 1)
+            {
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (str[i] == str[i - 1])
+                    {
+                        currentLength += 1;
+                    }
+                    else
+                    {
+                        actualMaxLength = currentLength;
+                        currentLength = 1;
+                    }
+                    if (actualMaxLength > maxLength)
+                    {
+                        maxLength = actualMaxLength;
+                    }
+                }
+            }
+            return maxLength;
+        }
+    }
+}
