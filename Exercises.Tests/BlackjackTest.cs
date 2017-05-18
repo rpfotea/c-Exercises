960249878ed6cc897216e6ab03f7270c0e29291e
Radu Fotea <petru.fotea@gmail.com>
2017-05-18 09:52:19 -0400

    Add Blackjack exercise + test

---
+++ Exercises.Tests/BlackjackTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class BlackjackT
+    {
+        [TestMethod]
+        public void BlackjackTest()
+        {
+            Exercises blackjack = new Exercises();
+
+            Assert.AreEqual(0, blackjack.Blackjack(22, 24));
+            Assert.AreEqual(21, blackjack.Blackjack(19, 21));
+            Assert.AreEqual(21, blackjack.Blackjack(21, 19));
+            Assert.AreEqual(19, blackjack.Blackjack(19, 22));
+            Assert.AreEqual(19, blackjack.Blackjack(22, 19));
+            Assert.AreEqual(19, blackjack.Blackjack(17, 19));
+        }
+    }
+}
