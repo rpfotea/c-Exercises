e7dff0025bcc0d64c7e4fc86e6c1570e1c73ee74
Radu Fotea <petru.fotea@gmail.com>
2017-05-19 11:15:54 -0400

    Add MaxSpan + tests

---
+++ Exercises.Tests/MaxSpanTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class MaxSpanT
+    {
+        [TestMethod]
+        public void MaxSpanTest()
+        {
+            Exercises maxSpan = new Exercises();
+            int[] nums1 = new int[0] { };
+            int[] nums2 = new int[1] { 4 };
+            int[] nums3 = new int[5] { 1, 2, 1, 1, 3 };
+            int[] nums4 = new int[7] { 1, 4, 2, 1, 4, 1, 4 };
+            int[] nums5 = new int[7] { 1, 4, 2, 1, 4, 4, 4 };
+            int[] nums6 = new int[10] { 9, 6, 8, 1, 4, 3, 1, 4, 4, 3 };
+
+            Assert.AreEqual(0, maxSpan.MaxSpan(nums1));
+            Assert.AreEqual(1, maxSpan.MaxSpan(nums2));
+            Assert.AreEqual(4, maxSpan.MaxSpan(nums3));
+            Assert.AreEqual(6, maxSpan.MaxSpan(nums4));
+            Assert.AreEqual(6, maxSpan.MaxSpan(nums5));
+            Assert.AreEqual(5, maxSpan.MaxSpan(nums6));
+        }
+    }
+}
---
+++ Exercises/MaxSpan.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+
+        /*
+        CHALLENGE: Consider the leftmost and righmost appearances of some value in an array.
+        We'll say that the "span" is the number of elements between the two inclusive. 
+        A single value has a span of 1. 
+        Returns the largest span found in the given array. (Efficiency is not a priority.)
+
+        maxSpan([1, 2, 1, 1, 3]) → 4
+        maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+        maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+        */
+        public int MaxSpan(int[] nums)
+        {
+            int finalMaxSpan = 0;
+            int actualMaxSpan = 0;
+            int actualSpan = 0;
+            if (nums.Length == 1)
+            {
+                finalMaxSpan = 1;
+            }
+            if (nums.Length > 1)
+            {
+                for(int i=0; i < nums.Length; i++)
+                {
+                    for(int j=i; j< nums.Length; j++)
+                    {
+                        if (nums[i] == nums[j])
+                        {
+                            //calculate actual span length betwen to identic numbers;
+                            actualSpan = j - i + 1;
+                            //if actual span is greater than the previous one, then keep the new one;
+                            if(actualSpan>actualMaxSpan)
+                            {
+                                actualMaxSpan = actualSpan;
+                            }
+                            //reset the actualSpan counter if is the last number from set;
+                            if (j == nums.Length - 1)
+                            {
+                                actualSpan = 0;
+                            }
+                        }
+                        if (actualMaxSpan > finalMaxSpan)
+                        {
+                            finalMaxSpan = actualMaxSpan;
+                        }
+                    }
+                }
+
+            }
+
+            return finalMaxSpan;
+        }
+    }
+}
