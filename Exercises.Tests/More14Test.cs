91ae0c891597dbce88eae649ab052aed35cd60e3
Radu Fotea <petru.fotea@gmail.com>
2017-06-05 10:53:59 -0400

    Add More14 exercise and test for it

---
+++ Exercises.Tests/More14Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class More14T
+    {
+        [TestMethod]
+        public void More14Test()
+        {
+            Exercises more14 = new Exercises();
+            int[] set1 = new int[3] { 1, 4, 1 };
+            int[] set2 = new int[4] { 1, 4, 1, 4 };
+            int[] set3 = new int[2] { 1, 1};
+
+            Assert.AreEqual(true, more14.More14(set1));
+            Assert.AreEqual(false, more14.More14(set2));
+            Assert.AreEqual(true, more14.More14(set3));
+
+        }
+    }
+}
---
+++ Exercises/More14.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+
+        /*
+
+        Given an array of ints, return true if the number of 1's is greater than the number of 4's
+
+        more14([1, 4, 1]) → true
+        more14([1, 4, 1, 4]) → false
+        more14([1, 1]) → true
+        */
+        public bool More14(int[] nums)
+        {
+            int ones = 0;
+            int fors = 0;
+            // loops true nums array
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //count number of 1's
+                if (nums[i] == 1)
+                {
+                    ones += 1;
+                }
+                //count number of 4's
+                if (nums[i] == 4)
+                {
+                    fors += 1;
+                }
+            }
+            //return true if number of 1's is grater than number of 4's
+            if(ones>fors)
+            {
+                return true;
+            }
+          
+            return false;
+        }
+    }
+}
