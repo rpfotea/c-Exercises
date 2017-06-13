5a5f86dfd97009f294534011149c3fff38efa080
Radu Fotea <petru.fotea@gmail.com>
2017-06-13 16:08:53 -0400

    add OneTwo exercise + test for it

---
+++ Exercises.Tests/OneTwoTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class OneTwoT
+    {
+        [TestMethod]
+        public void OneTwoTest()
+        {
+            Exercises oneTwo = new Exercises();
+            string input1 = "abc";
+            string input2 = "tca";
+            string input3 = "tcagdo";
+
+            string output1 = oneTwo.OneTwo(input1);
+            string output2 = oneTwo.OneTwo(input2);
+            string output3 = oneTwo.OneTwo(input3);
+
+            Assert.AreEqual("bca", output1);
+            Assert.AreEqual("cat", output2);
+            Assert.AreEqual("catdog", output3);
+
+        }
+    }
+}
---
+++ Exercises/OneTwo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+        * Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". 
+        * Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". 
+        * Ignore any group of fewer than 3 chars at the end.
+        *
+        * oneTwo("abc") → "bca"
+        * oneTwo("tca") → "cat"
+        * oneTwo("tcagdo") → "catdog"
+        */
+        public string OneTwo(string str)
+        {
+            string output = "";
+            string changeLetter = "";
+            if (str.Length > 2 && str.Length % 3 == 0)
+            {
+                for (int i = 0; i < str.Length - 2; i += 3)
+                {
+                    changeLetter = str.Substring(i, 1);
+                    //for first iteration will use initial string
+                    if (i < 1)
+                    {
+                        output = str.Insert(i + 3, changeLetter);
+                        output = output.Remove(i, 1);
+                    }
+                    //starting with second iteration will use thre previous output
+                    else
+                    {
+                        output = output.Insert(i + 3, changeLetter);
+                        output = output.Remove(i, 1);
+                    }
+                }
+                return output;
+            }
+            return str;
+        }
+    }
+}
