9b1046b2a18f9b3e9fd91677b3d83bcc084da89a
Radu Fotea <petru.fotea@gmail.com>
2017-06-15 10:37:45 -0400

    Add RepeatSeparator exerc + test for it

---
+++ Exercises.Tests/RepeatSeparatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class RepeatSeparatorT
+    {
+        [TestMethod]
+        public void RepeatSeparatorTest()
+        {
+            Exercises repeatSep = new Exercises();
+
+            string expect1 = "WordXWordXWord";
+            string expect2 = "ThisAndThis";
+            string expect3 = "This";
+
+            Assert.AreEqual(expect1, repeatSep.RepeatSeparator("Word", "X", 3));
+            Assert.AreEqual(expect2, repeatSep.RepeatSeparator("This", "And", 2));
+            Assert.AreEqual(expect3, repeatSep.RepeatSeparator("This", "And", 1));
+        }
+    }
+}
---
+++ Exercises/RepeatSeparator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+        * Given two strings, word and a separator sep, return a big string made of count occurrences 
+        * of the word, separated by the separator string.
+        *
+        *
+        * repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+        * repeatSeparator("This", "And", 2) → "ThisAndThis"
+        * repeatSeparator("This", "And", 1) → "This"
+        * 
+        */
+        public string RepeatSeparator(string word, string sep, int count)
+        {
+            string output = "";
+            for(int i=0; i<count; i++)
+            {
+                if(i>0)
+                {
+                    output += sep;
+                }
+                output += word;
+            }
+            return output;
+        }
+    }
+}
